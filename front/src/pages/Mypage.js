a9b65ee396f5b35cb468b25d69a1299307a106ea
yurimn <leeyou6757@gmail.com>
2022-08-21 06:22:47 +0900

    마이페이지 수정

--- front/src/pages/Mypage.js
+++ front/src/pages/Mypage.js
@@ -6,6 +6,10 @@ import palette from "../styles/pallete";
 import "./Mypage.css";
 
 const Mypage = ({ user }) => {
+<<<<<<< Updated upstream
+=======
+	const isSeat = true;
+>>>>>>> Stashed changes
 	return (
 		<div className="my-page">
 			<img src={user.photoURL} alt="" />
@@ -20,6 +24,31 @@ const Mypage = ({ user }) => {
 					<span>3</span>
 				</div>
 			</div>
+<<<<<<< Updated upstream
+=======
+			<div id="blank">
+				<HorizontalFlex>
+					<Header>
+						현재 <PurpleStyle>내 자리</PurpleStyle>
+					</Header>
+				</HorizontalFlex>
+			</div>
+			<div id="my-page-seat">
+				{isSeat ? (
+					<div id="my-content">
+						<Station /> 외선순환 4-2번 칸 5번 좌석
+					</div>
+				) : (
+					<div id="my-content2">
+						현재 자리에 앉아 있지 않습니다!
+						<Link to="/sit">
+							<Button box_shadow>자리 등록하기</Button>
+						</Link>
+					</div>
+				)}
+			</div>
+
+>>>>>>> Stashed changes
 			<button className="button-signout" onClick={() => auth.signOut()}>
 				로그아웃하기
 			</button>
