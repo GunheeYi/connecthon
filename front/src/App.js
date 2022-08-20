50fa0e29a312e43e0e5a035a579c1a76fef003dd
ori5r2 <96720326+ori5r2@users.noreply.github.com>
2022-08-20 16:29:37 +0900

    empty row

--- front/src/App.js
+++ front/src/App.js
@@ -1,35 +1,33 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import Login from './components/Login';
-import Home from './components/Home';
-import firebase from './services/firebase';
+import Login from "./components/Login";
+import Home from "./components/Home";
+import firebase from "./services/firebase";
 
-import Button from './components/Button';
-import EmptyRow from './components/EmptyRow';
-import EntranceNum from './components/EntranceNum';
-import GlobalStyle from './styles/GlobalStyle'
+import Button from "./components/Button";
+import EmptyRow from "./components/EmptyRow";
+import EntranceNum from "./components/EntranceNum";
+import GlobalStyle from "./styles/GlobalStyle";
 
 function App() {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    firebase.auth().onAuthStateChanged(user => {
+    firebase.auth().onAuthStateChanged((user) => {
       setUser(user);
-    })
-  }, [])
+    });
+  }, []);
 
   console.log(user);
 
   return (
     <div className="app">
-      <GlobalStyle/>
+      <GlobalStyle />
       {user ? <Home user={user} /> : <Login />}
-
-      <Button>시작하기</Button>
-      <EntranceNum text={"4-2"}/>
+      <EmptyRow />
+      {/* <Button>시작하기</Button>
+      <EntranceNum text={"4-2"} /> */}
     </div>
-
-    
   );
 }
 
--- front/src/components/EmptyRow.js
+++ front/src/components/EmptyRow.js
@@ -1,9 +1,37 @@
-import React from 'react';
-import styled, { css } from 'styled-components';
-import palette from '../styles/pallete';
+import React from "react";
+import styled, { css } from "styled-components";
+import palette from "../styles/pallete";
+import EntranceNum from "./EntranceNum";
+
+const EmptyRowStyle = styled.div`
+  width: 334px;
+  height: 88px;
+  // background-color: white;
+
+  // 정렬
+  display: flex;
+  align-items: center;
+`;
+
+const SubInfoStyle = styled.div`
+  width: 314px;
+  height: 88px;
+  border-radius: 16px;
+  border: 1px solid ${palette.light_gray_3};
+  //background-color: ${palette.main_purple};
+
+  // 정렬
+  position: relative;
+  margin-left: auto;
+`;
 
 const EmptyRow = () => {
-  return <div></div>;
+  return (
+    <EmptyRowStyle>
+      <EntranceNum text="4-2" />
+      <SubInfoStyle />
+    </EmptyRowStyle>
+  );
 };
 
 export default EmptyRow;
